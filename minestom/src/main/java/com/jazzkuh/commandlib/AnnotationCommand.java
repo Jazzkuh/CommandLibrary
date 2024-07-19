9e7e93f606d92e6c5317b97322c191cb3ae75271
Daan Plugge <35238334+TheBathDuck@users.noreply.github.com>
2024-07-19 23:04:06 +0200

    fix minestom parsing

--- minestom/src/main/java/com/jazzkuh/commandlib/AnnotationCommand.java
+++ minestom/src/main/java/com/jazzkuh/commandlib/AnnotationCommand.java
@@ -10,7 +10,6 @@ import net.minestom.server.command.CommandManager;
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
-import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.arguments.ArgumentLiteral;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 
@@ -46,7 +45,7 @@ public class AnnotationCommand extends Command implements AnnotationCommandImpl 
     }
 
     public void execute(CommandSender sender, CommandContext context) {
-        String[] args = context.getMap().values().stream().map(Object::toString).toArray(String[]::new);
+        String[] args = Arrays.stream(context.getInput().split(" ")).skip(1).toArray(String[]::new);
 
         if (args.length < 1) {
             if (this.mainCommand == null) {
@@ -59,7 +58,8 @@ public class AnnotationCommand extends Command implements AnnotationCommandImpl 
         }
 
         for (AnnotationSubCommand subCommand : subCommands) {
-            if (!args[0].equalsIgnoreCase(subCommand.getName()) && !subCommand.getAliases().contains(args[0].toLowerCase())) continue;
+            if (!args[0].equalsIgnoreCase(subCommand.getName()) && !subCommand.getAliases().contains(args[0].toLowerCase()))
+                continue;
             this.executeCommand(subCommand, sender, args);
             return;
         }
@@ -79,10 +79,14 @@ public class AnnotationCommand extends Command implements AnnotationCommandImpl 
         AnnotationCommandExecutor.CommandResult commandResult = commandExecutor.execute(commandSender, args);
         switch (commandResult) {
             case NOT_ENOUGH_ARGUMENTS -> this.formatUsage(sender);
-            case ERROR -> sender.sendMessage(Component.text("An error occurred while executing this subcommand.", TextColor.fromHexString("#FB465C")));
-            case NOT_ALLOWED -> sender.sendMessage(Component.text("You are not allowed to execute this command.", TextColor.fromHexString("#FB465C")));
-            case CONTEXT_RESOLVER_NOT_FOUND -> sender.sendMessage(Component.text("A context resolver was not found for one of the parameters.", TextColor.fromHexString("#FB465C")));
-            case PARAMETER_INVALID -> sender.sendMessage(Component.text("One of the parameters was invalid.", TextColor.fromHexString("#FB465C")));
+            case ERROR ->
+                    sender.sendMessage(Component.text("An error occurred while executing this subcommand.", TextColor.fromHexString("#FB465C")));
+            case NOT_ALLOWED ->
+                    sender.sendMessage(Component.text("You are not allowed to execute this command.", TextColor.fromHexString("#FB465C")));
+            case CONTEXT_RESOLVER_NOT_FOUND ->
+                    sender.sendMessage(Component.text("A context resolver was not found for one of the parameters.", TextColor.fromHexString("#FB465C")));
+            case PARAMETER_INVALID ->
+                    sender.sendMessage(Component.text("One of the parameters was invalid.", TextColor.fromHexString("#FB465C")));
         }
     }
 
@@ -99,7 +103,8 @@ public class AnnotationCommand extends Command implements AnnotationCommandImpl 
                     continue;
                 }
 
-                if (commandSender.getSender().hasPermission(subCommand.getPermission())) options.add(subCommand.getName());
+                if (commandSender.getSender().hasPermission(subCommand.getPermission()))
+                    options.add(subCommand.getName());
             }
 
             return StringUtils.copyPartialMatches(args[0], options, new ArrayList<>(options.size())).stream().map(ArgumentType::Literal).toList();
@@ -112,11 +117,13 @@ public class AnnotationCommand extends Command implements AnnotationCommandImpl 
                     continue;
                 }
 
-                if (commandSender.getSender().hasPermission(subCommand.getPermission())) options.add(subCommand.getName());
+                if (commandSender.getSender().hasPermission(subCommand.getPermission()))
+                    options.add(subCommand.getName());
                 continue;
             }
 
-            if (!args[0].equalsIgnoreCase(subCommand.getName()) && !subCommand.getAliases().contains(args[0].toLowerCase())) continue;
+            if (!args[0].equalsIgnoreCase(subCommand.getName()) && !subCommand.getAliases().contains(args[0].toLowerCase()))
+                continue;
             AnnotationCommandExecutor<CommandSender> subCommandExecutor = new AnnotationCommandExecutor<>(subCommand, this);
             options.addAll(subCommandExecutor.complete(commandSender, args));
         }
