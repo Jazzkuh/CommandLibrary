612d042fb8aef0c8f640f5c28a2f7a512bfa1845
Jazzkuh <jazzbauer77@gmail.com>
2024-07-25 18:11:39 +0200

    Add terminal support

--- minestom/src/main/java/com/jazzkuh/commandlib/minestom/MinestomCommandLoader.java
+++ minestom/src/main/java/com/jazzkuh/commandlib/minestom/MinestomCommandLoader.java
@@ -4,10 +4,19 @@ import com.jazzkuh.commandlib.common.resolvers.CompletionResolverRegistry;
 import com.jazzkuh.commandlib.common.resolvers.ContextResolverRegistry;
 import com.jazzkuh.commandlib.minestom.resolvers.GameModeResolver;
 import com.jazzkuh.commandlib.minestom.resolvers.PlayerResolver;
+import com.jazzkuh.commandlib.minestom.terminal.MinestomTerminal;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
 
 public class MinestomCommandLoader {
+    public static void startTerminal() {
+        MinestomTerminal.start();
+    }
+
+    public static void stopTerminal() {
+        MinestomTerminal.stop();
+    }
+
     public static void loadResolvers() {
         ContextResolverRegistry.registerResolver(Player.class, new PlayerResolver());
         ContextResolverRegistry.registerResolver(GameMode.class, new GameModeResolver());
